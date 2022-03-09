b8d0258d2cf2d9d40c75036105713d490cda8f20
Saulo-pedro <79072631+Saulo-pedro@users.noreply.github.com>
2022-03-09 09:19:37 -0300

    Update Video.cs

--- site/Video.cs
+++ site/Video.cs
@@ -1,26 +1,45 @@
 using System;
 
 class Video{
+  private int id;
   private string nome;
   private DateTime duracao;
-
-  public Video(string nome, DateTime duracao){
+  private int idPlaylist;
+  
+  public Video(int id){
+    this.nome = nome;
+  }
+  public Video(int id, string nome, DateTime duracao, int idPlaylist){
     this.nome = nome;
     this.duracao = duracao;
+    this.id = id;
+    this.idPlaylist = idPlaylist;
   }
    public void Setnome(string nome){
     this.nome = nome;
   }
-  public void Setduracao(DateTime duracao){
+  public void SetId(int id){
+    this.id = id;
+  }
+  public void SetDuracao(DateTime duracao){
     this.duracao = duracao;
+  }
+  public void SetIdPlaylist(int idPlaylist){
+    this.idPlaylist = idPlaylist;
   }
   public string Getnome(){
     return nome;
+  }
+  public int GetId(){
+    return id;
+  }
+  public int GetIdPLaylist(){
+    return idPlaylist;
   }
   public DateTime GetDuracao(){
     return duracao;
   }
   public override string ToString(){
-    return $"{nome} - {duracao: HH:mm:ss}";
+    return $"{id} - {nome} - {duracao: HH:mm:ss} - {idPlaylist}";
   }
 }
