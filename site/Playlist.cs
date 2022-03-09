8feee41eb9a72cabda57c3ec71302d4815eb00d5
47380 <97446480+47380@users.noreply.github.com>
2022-03-09 09:39:11 -0300

    Update Playlist.cs

--- site/Playlist.cs
+++ site/Playlist.cs
@@ -1,34 +1,45 @@
 using System;
 
-class Playlist{
+class Video{
   private int id;
   private string nome;
-  private string nomeVi;
-
-  public Playlist(string nome, string nomeVi, int id){
+  private DateTime duracao;
+  private int idPlaylist;
+  
+  public Video(int id){
     this.nome = nome;
-    this.nomeVi= nomeVi;
-    this.id = id;
   }
-  public void Setnome(string nome){
+  public Video(int id, string nome, DateTime duracao, int idPlaylist){
+    this.nome = nome;
+    this.duracao = duracao;
+    this.id = id;
+    this.idPlaylist = idPlaylist;
+  }
+   public void Setnome(string nome){
     this.nome = nome;
   }
   public void SetId(int id){
     this.id = id;
   }
-  public void SetnomeVi(string nomeVi){
-    this.nomeVi = nomeVi;
+  public void SetDuracao(DateTime duracao){
+    this.duracao = duracao;
+  }
+  public void SetIdPlaylist(int idPlaylist){
+    this.idPlaylist = idPlaylist;
   }
   public string Getnome(){
     return nome;
-    }
-  public string GetnomeVi(){
-    return nomeVi;
-    }
+  }
   public int GetId(){
     return id;
   }
+  public int GetIdPLaylist(){
+    return idPlaylist;
+  }
+  public DateTime GetDuracao(){
+    return duracao;
+  }
   public override string ToString(){
-    return $"{id} - {nome} - {nomeVi}";
+    return $"{id} - {nome} - {duracao: HH:mm:ss} - {idPlaylist}";
   }
 }
