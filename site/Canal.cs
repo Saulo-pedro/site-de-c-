15098e225e790ca60a36cf30ec25b4d0cae080d2
47380 <97446480+47380@users.noreply.github.com>
2022-03-21 10:19:16 -0300

    Update Canal.cs

--- site/Canal.cs
+++ site/Canal.cs
@@ -1,18 +1,6 @@
 using System;
 
 class Canal{
-  private string Canal;
-
-  public Canal(string canal){
-    this.Canal = canal;
-  }
-  public void SetCanal(){
-    this.Canal = canal;
-  }
-  public string GetCanal(){
-    return canal;
-  }
-  public override string ToString(){
-    return $"{canal}";
-  }
+  public int Id { get; set; }
+  public string Nome { get ; set; }
 }
